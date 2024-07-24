7bc4faa8d5447902c5e22121ddfe3d7e1dc58ee2
bhavya <bhavya4official@gmail.com>
2024-07-24 11:45:32 +0530

    all files added

--- tests/UIbasicsTest.spec.js
+++ tests/UIbasicsTest.spec.js
@@ -26,12 +26,26 @@ test("Page Context playwright test", async ({ page }) => {
 test("Login page test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
+
+  const username = page.locator("#username");
+  const signIn = page.locator("#signInBtn");
+  const cardTitle = page.locator(".card-body a");
+
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 
   //Playwright suggest to use CSS locator instead of xpath - #id .class [attribute='value']
-  await page.locator("#username").fill("rahulshetty");
+  await username.fill("rahulshetty");
   await page.locator("[type='password']").fill("learning");
-  await page.locator("#signInBtn").click();
+  await signIn.click();
+  //Playwright automatically wait until this locator shown up on page - no implicit wait needed
+  await expect(page.locator("[style*='block']")).toContainText("Incorrect"); //negative scenario
+
+  await username.fill("rahulshettyacademy");
+  await signIn.click();
+  console.log(await cardTitle.first().textContent()); //first element - This method/action will wait 30s timeout until element shows up
+  console.log(await cardTitle.nth(1).textContent()); //second element
+
+  console.log(await cardTitle.allTextContents()); //This method/action does not wait 30s timeout (until all elements are showing up)
 });
 
 /*
