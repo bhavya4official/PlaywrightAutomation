46aa83d2dd7bc63e50d2b96d773ecb226c6a15b7
bhavya <bhavya4official@gmail.com>
2024-07-29 17:01:09 +0530

    E2E

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -5,9 +5,26 @@ test("Wait dynamically for new page in service based app", async ({ page }) => {
   await page.locator("#userEmail").fill("bhavya4official@gmail.com");
   await page.locator("#userPassword").fill("Test@123");
   await page.locator("[value='Login']").click();
-  
+
   //await page.waitForLoadState("networkidle"); //wait until network comes to idle state - So all elements are fully loaded
   await page.locator(".card-body b").first().waitFor(); //Recommanded wait instead of networkidle - to wait until element filly loaded
   const titles = await page.locator(".card-body b").allTextContents();
   console.log(titles);
+});
+
+test("E-commerce E2E work flow", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill("bhavya4official@gmail.com");
+  await page.locator("#userPassword").fill("Test@123");
+  await page.locator("[value='Login']").click();
+
+  const product = page.locator(".card-body");
+  const productName = "Zara Coat 3";
+
+  for (let i = 0; i < (await product.count()); i++) {
+    if ((await product.nth(i).locator("b")) === productName) {
+      await product.nth(i).locator("text= Add To Cart").click();
+      break;
+    }
+  }
 });
